f87fbd541e2c6f528c23dec210386117b39be48e
Thonwelling <thonwellingdani13@gmail.com>
2023-10-03 15:19:10 -0300

    Project Created

---
+++ src/main/java/com/ambarx/AliExpress/AliExpressApplication.java
@@ -0,0 +1,13 @@
+package com.ambarx.AliExpress;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AliExpressApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AliExpressApplication.class, args);
+	}
+
+}
